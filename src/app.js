fe782c93ff09a86537506d6bdc3fa37aa107cde7
efegabo <suarezjesus1123@gmail.com>
2024-04-21 19:35:59 -0500

    puerto del servidor

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//imports
+import express from 'express'
+
+
+//initialitationes 
+const app = express()
+const PORT = process.env.PORT||3000;
+//setting
+rm
+
+
+//port
+app.listen(PORT,()=>{
+    console.log("server on port:", PORT)
+})
